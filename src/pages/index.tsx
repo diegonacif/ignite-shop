304ba73096a865ae9ae501a23541628894942a2c
diegonacif <diegoreisnacif@gmail.com>
2023-07-09 21:22:15 -0300

    chore: configuring stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
+import { styled } from "../styles"
+
+export const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+  transition: 'filter 0.1s',
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+    transition: 'filter 0.2s',
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <Button>Enviar!</Button>
   )
 }
