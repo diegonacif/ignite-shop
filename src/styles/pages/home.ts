3cf935172e0d5b9e6b4100cfc25d219e1c7e11a0
diegonacif <diegoreisnacif@gmail.com>
2023-07-13 17:23:17 -0300

    feat: keen-slider carousel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,8 +2,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
-  // backgroundColor: '#4287f5',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   minHeight: 656,
