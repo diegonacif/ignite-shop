00297f2e4e8791671319db76cc5a8fad2ed993d4
diegonacif <diegoreisnacif@gmail.com>
2023-07-14 17:03:38 -0300

    refactor: using ssg - static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,9 @@ import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import { useKeenSlider } from "keen-slider/react"
-
-// import camiseta1 from '../assets/camisetas/1.png'
-// import camiseta2 from '../assets/camisetas/2.png'
-// import camiseta3 from '../assets/camisetas/3.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -56,7 +51,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -76,6 +71,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
