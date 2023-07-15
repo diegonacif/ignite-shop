62a7939bcf5f77fb65468ba17392c1d8a85d5fdf
diegonacif <diegoreisnacif@gmail.com>
2023-07-15 12:50:15 -0300

    feat: stripe checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,14 +12,13 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
-  const { isFallback } = useRouter();
-
-  if (isFallback) {
-    return <p>Loading...</p>
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
   }
   
   return (
@@ -39,7 +38,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -76,6 +75,7 @@ export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params
           price.unit_amount ? price.unit_amount / 100 : 0
         ),
         description: product.description, 
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1, // 1 hours
